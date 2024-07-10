1936f2eef70952c5b7d5f2799a95612fda1d983f
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2024-07-09 19:36:33 -0600

    add ace combat factions and scenario

--- packages/apps/campaign/src/data/factions/index.ts
+++ packages/apps/campaign/src/data/factions/index.ts
@@ -14,6 +14,17 @@ import * as usa72 from "./usa-1972.json";
 import * as usa77 from "./usa-1977.json";
 import * as usa04 from "./usa-2004.json";
 
+import * as belka95 from "./belka-1995.json";
+import * as ustio95 from "./ustio-1995.json";
+import * as isaf04 from "./isaf-2004.json";
+import * as erusea04 from "./erusea-2004.json";
+import * as isaf05 from "./isaf-2005.json";
+import * as erusea05 from "./erusea-2005.json";
+import * as osea10 from "./osea-2010.json";
+import * as yuktobania10 from "./yuktobania-2010.json";
+import * as osea19 from "./osea-2019.json";
+import * as erusea19 from "./erusea-2019.json";
+
 export const factionList: Array<Types.Campaign.Faction> = [
 	Types.Campaign.Schema.faction.parse(usa52),
 	Types.Campaign.Schema.faction.parse(russia55),
@@ -28,4 +39,14 @@ export const factionList: Array<Types.Campaign.Faction> = [
 	Types.Campaign.Schema.faction.parse(usnavy87),
 	Types.Campaign.Schema.faction.parse(russia96),
 	Types.Campaign.Schema.faction.parse(usa04),
+	Types.Campaign.Schema.faction.parse(belka95),
+	Types.Campaign.Schema.faction.parse(ustio95),
+	Types.Campaign.Schema.faction.parse(isaf04),
+	Types.Campaign.Schema.faction.parse(erusea04),
+	Types.Campaign.Schema.faction.parse(isaf05),
+	Types.Campaign.Schema.faction.parse(erusea05),
+	Types.Campaign.Schema.faction.parse(osea10),
+	Types.Campaign.Schema.faction.parse(yuktobania10),
+	Types.Campaign.Schema.faction.parse(osea19),
+	Types.Campaign.Schema.faction.parse(erusea19),
 ];
